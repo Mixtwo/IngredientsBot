4ac314f2ccfb9cbd25d0ae61d01645ce57d9aaa8
Mimi Lin <mlingao92@gmail.com>
2017-11-09 01:23:21 -0800

    Add files via upload
    First Commit

---
+++ bot.py
@@ -0,0 +1,91 @@
+import praw
+import config
+import time
+import os
+import re
+from ingredients_methods import product_info_for
+
+# Login
+def bot_login():
+	r = praw.Reddit(username = config.username, 
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "skin bot test v0.1")
+
+	return r
+# extract img
+def extract_img(url):
+	word = ".jpg"
+	img_url = re.match(r'(.*(.jpg|.jpeg|.gif|.png))', url).group(1)
+	return img_url
+
+# Runs the bot
+def run_bot(r, comments_replied_to):
+	print ("Obtaining 25 comments...")
+
+	for comment in r.subreddit('test').comments(limit=25):
+		if ("!ingredients" in comment.body or "!Ingredients" in comment.body) and comment.id not in comments_replied_to:
+			print("String with !ingredients found!")
+			print("Posted by: " + comment.author.name)
+			print("Comment Body: " + comment.body)
+
+			# disect comment body for query words between < > ? fo non-greedy
+			search_terms = re.search(r'<(\w+( \w+)*)>', comment.body) #.group(1)
+
+			if search_terms:
+				search_terms = search_terms.group(1)
+
+				############### implementation of ingredients_methods.py ##################
+
+				product_info = product_info_for(search_terms)
+
+				if product_info != None:
+					product_list_str = ' '.join(product_info.ingredients)
+					product_img = extract_img(product_info.img_url)
+					print(product_info.brand)
+					print(product_info.name)
+					print(product_img)
+					print(product_list_str)
+
+					comment.reply("#####" + product_info.brand + " - " + product_info.name + "\n" + "#####[[Product Image]](" + product_img + ")" + "\n" + "---" + "\n" + "#####INGREDIENTS: " + "\n>" + product_list_str + "\n" + "\n" + "\n" + "---" + "\n" + "\n" + "^^| ^^If ^^you ^^would ^^like ^^to ^^use ^^me, ^^the ^^command ^^is: ^^**!ingredients** ^^**<detailed** ^^**product** ^^**name>** ^^| ^^Message ^^me ^^for ^^complaints ^^and ^^suggestions ^^|")
+
+				else:
+
+					comment.reply("Sorry, searching for [" + search_terms + "] produced no results.")
+
+				###########################################################################
+
+			else:
+				comment.reply("Sorry, I seem to have issues with the summoning command. It should be **!ingredients <*your search terms*>**")
+
+			print("Replied to comment " + comment.id)
+
+			# save comment id to our comments replied to Id txt file
+			comments_replied_to.append(comment.id)
+			with open("comments_replied_to_ID.txt", "a") as f:
+				f.write(comment.id + "\n")
+
+	print ("Sleeping for 10 seconds...")		
+	time.sleep(10)	
+
+# Saves comment ID to a file
+def get_saved_comment_IDs():
+	if not os.path.isfile("comments_replied_to_ID.txt"):
+		comments_replied_to_ID = []
+
+	else:	
+		with open("comments_replied_to_ID.txt", "r") as f:
+			comments_replied_to_ID = f.read()
+			comments_replied_to_ID = comments_replied_to_ID.split("\n")		
+			# filter object needs to be turned into a list manually in python 3
+			comments_replied_to_ID = list(filter(None, comments_replied_to_ID))
+
+	return comments_replied_to_ID
+
+# main
+r = bot_login()
+comments_replied_to = get_saved_comment_IDs()
+
+while True:
+	run_bot(r, comments_replied_to)
---
+++ ingredients_methods.py
@@ -0,0 +1,94 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Product:
+
+	def __init__(self, brand, name, img_url, product_url, ingredients):
+		self.brand = brand
+		self.name = name
+		self.img_url = img_url
+		self.product_url = product_url
+		self.ingredients = ingredients
+
+###################### search results page ################################
+
+# queries = ["cerave", "moisturizing", "pm", "lotion"]
+
+def get_first_search_result_soup(search_return_list):
+		j = 0
+		while j<len(search_return_list):
+			soup_brand = search_return_list[j].find(attrs={'class': 'review-brand'}).text.strip()
+			if soup_brand != "Beautypedia: Exclusives":
+				return search_return_list[j]
+			j+=1
+
+def get_seach_results_page(queries):
+	queries_str = queries.replace(' ', '+').lower()
+	print(queries_str)
+
+	search_url = "https://www.beautypedia.com/skin-care-reviews/?Ntt=" + queries_str
+	html_search = urllib.request.urlopen(search_url)
+	soup_search = BeautifulSoup(html_search, 'html.parser')
+	search_return_list = soup_search.find_all('div', attrs={'class': 'review-col col-2'})
+	
+	#########
+	zero_results = soup_search.find_all('div', attrs={'class': 'non-zero-results'})
+
+	for false_results in zero_results:
+		print("PRODUCT FOUND!")
+		soup_single = get_first_search_result_soup(search_return_list)
+		product_url = "https://www.beautypedia.com" + soup_single.find("a", href=True, attrs={'class': 'review-product'})['href']
+
+		print(product_url)
+		return product_url
+	#########
+	print("NO PRODUCT FOUND")
+	return None
+
+####################### product parsed page #################################
+
+def get_soup_from_results_page(product_url):
+	htmlfile = urllib.request.urlopen(product_url)
+	soup = BeautifulSoup(htmlfile, 'html.parser')
+	return soup
+
+def product_info(soup, product_url):
+
+	# brand name
+	brand_box = soup.find(attrs={'class': 'brand-name'})
+	brand = brand_box.text.strip()
+	# print(brand)
+
+	# product name
+	product_box = soup.find(attrs={'class': 'product-name'})
+	name = product_box.text.strip()
+	# print(name)
+
+	# product image link
+	product_img_box = soup.find(attrs={'class': 'product-image'}).find("img")
+	product_img = product_img_box["src"]
+	# print(product_img)
+
+	# ingredient list
+	ingredients_box = soup.find(attrs={'class': 'content-item ingredients'})
+	ingredients_text = ingredients_box.text.strip()
+	ingredients = ingredients_text.split()
+	# print(ingredients)
+
+	product_details = Product(brand, name, product_img, product_url, ingredients)
+
+	return product_details
+
+######################### single methods for this class ##########################
+
+def product_info_for(queries):
+	product_url = get_seach_results_page(queries)
+
+	if product_url == None:
+		return None
+
+	soup = get_soup_from_results_page(product_url)
+	return product_info(soup, product_url)
+
+######################## main ##############################################
+
